a5eb658d31127ccfe18ef1fb6dae45d63dc5c0f5
nagesh26730 <80949920+nagesh26730@users.noreply.github.com>
2021-11-27 22:56:51 -0800

    Update tomcatDeloy.groovy

--- vars/tomcatDeloy.groovy
+++ vars/tomcatDeloy.groovy
@@ -1,5 +1,5 @@
 
-def call(ip,credsID,myName){
+def call(ip,credsID,warName){
   sshagent(["${credsID}"]) {
       sh "scp -o StrictHostKeyChecking=no target/{warName}.war ec2-user@{ip}:/opt/tomcat/webapps/"
       sh "ssh ec2-user@{ip} /opt/tomcat/bin/shutdown.sh"
