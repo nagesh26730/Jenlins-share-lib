1782f6b9ea78a4af767b968c7c73f27b3f00a6cd
nagesh26730 <80949920+nagesh26730@users.noreply.github.com>
2021-11-30 00:19:06 -0800

    Update mavennexusupload.groovy

--- mavennexusupload.groovy
+++ mavennexusupload.groovy
@@ -6,6 +6,6 @@ def artifactId = pomFile.artifactId
 def groupId = pomFile.groupId
 def type = pomFile.packaging
   
-  nexusArtifactUploader artifacts: [[artifactId: "{artifactId}", classifier: '', file: "target/${artifactId}-${version}.war", type: type]], credentialsId: 'nexus3', groupId: groupId, nexusUrl: '15.207.100.32:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: "repoName", version: "${version}"
+  nexusArtifactUploader artifacts: [[artifactId: "$ {artifactId}", classifier: '', file: "target/${artifactId}-${version}.war", type: type]], credentialsId: 'nexus3', groupId: groupId, nexusUrl: '15.207.100.32:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: repoName, version: "${version}"
             
 }
